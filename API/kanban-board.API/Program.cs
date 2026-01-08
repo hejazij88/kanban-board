de07bdb9d1a341d6cff16ea036d7462a9c626cd7
98913 <98913@LAPTOP-JQR66TAF>
2026-01-08 11:09:32 +0330

    Use SqlServer

--- API/kanban-board.API/Program.cs
+++ API/kanban-board.API/Program.cs
@@ -1,6 +1,15 @@
+using kanban_board.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                       ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+
+builder.Services.AddDbContext<KanbanBoardContext>(options =>
+    options.UseSqlServer(connectionString));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
