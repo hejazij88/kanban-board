f3b8aec0daa34a20bfe00a7825f5383794c92f4b
98913 <98913@LAPTOP-JQR66TAF>
2026-02-07 11:31:11 +0330

    Add Class ApplicationUser

--- Infrastructure/Kanban-Board.Infrastructure/Identity/ApplicationUser.cs
+++ Infrastructure/Kanban-Board.Infrastructure/Identity/ApplicationUser.cs
@@ -1,6 +1,9 @@
-﻿namespace Kanban_Board.Domain.Models;
+﻿using Kanban_Board.Domain.Models;
+using Microsoft.AspNetCore.Identity;
 
-public class ApplicationUser
+namespace Kanban_Board.Infrastructure.Identity;
+
+public class ApplicationUser: IdentityUser<int>
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
